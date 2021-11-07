6dc8ffd4f0f8ad1c61e5abf855f37c368cc9a3fc
root <root@WINAP6lSE8VfKVX.corp.avanade.org>
2021-11-06 21:51:24 -0300

    encoding xml response

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import "github.com/gin-gonic/gin"
+import "encoding/xml"
+
+type Person struct {
+	XMLName xml.Name `xml:"person"`
+	FirstName string `xml:"firstName,attr"`
+	LastName string `xml:"lastName,attr"`
+}
+
+func IndexHandler(c *gin.Context) {
+	c.XML(200, Person{FirstName: "Leandro", LastName: "Lopes"})
+}
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello world",
-		})
-	})
+	router.GET("/", IndexHandler)
 	router.Run(":5000")
 }
